8dd6e71a6387f5b58aa85cc53d548e02aa1b6623
Jie-Wei <swiftywu@yahoo.com>
2018-01-30 19:18:26 -0500

    Add change_size and add_suger methods of Drink class

--- drink.py
+++ drink.py
@@ -1,3 +1,10 @@
 class Drink:
     def __init__(self, size="small"):
         self.size = size
+        self.suger = 2
+
+    def change_size(self, size):
+        self.size = size
+
+    def add_suger(self, amount):
+        self.suger += amount
